d6cce75858f147a57721ab634cb671006924f9c3
Lera Green <zelnayavaleriya@gmail.com>
2018-03-31 15:44:35 +0300

    делает чо-то, но всё ломается

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,19 @@
+const appContainer = document.querySelector(`.app`);
+
 export const getElementFromTemplate = (string) => {
   const template = document.createElement(`template`);
   template.innerHTML = string;
   return template.content;
+};
+
+export const showScreen = (element) => {
+  const mainClass = '.main';
+  const main = appContainer.querySelector(mainClass);
+  if (main) {
+    appContainer.replaceChild(element, main);
+  } else {
+    appContainer.insertBefore(element, appContainer.children[0]);
+  }
 };
 
 // спросить как ещё лучше делать, если есть шаблонные строки
