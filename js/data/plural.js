358a8f901a2b68885857b618f4196097723949e3
Lera Green <zelnayavaleriya@gmail.com>
2018-04-10 23:05:06 +0300

    добавляет плюрализатор и тесты к нему

---
+++ js/data/plural.js
@@ -0,0 +1,24 @@
+const WORDS_LENGTH = 3;
+
+export default (number, words) => {
+  if (words.length !== WORDS_LENGTH) {
+    return `${number}`;
+  }
+  let numeral = words[1];
+
+  if (number >= 11 && number <= 14) {
+    numeral = words[0];
+  } else {
+    const remainder = number % 10;
+    if (!remainder) {
+      numeral = words[0];
+    } else if (remainder >= 2 && remainder <= 4) {
+      numeral = words[2];
+    } else if (remainder >= 5 && remainder <= 9) {
+      numeral = words[0];
+    }
+  }
+
+  const string = `${number} ${numeral}`;
+  return string;
+};
