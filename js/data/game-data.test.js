75748d999058a7392a86f6d769778412d80378f5
Lera Green <zelnayavaleriya@gmail.com>
2018-05-05 17:12:16 +0300

    фиксит тесты

--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -19,39 +19,40 @@ describe(`check answer type`, () => {
 
 
 describe(`points from answers`, () => {
+  const testQuestions = [{'question': 12}, {'question': 13}, {'question': 12}];
+
   it(`should return 2 if answer is fast`, () => {
-    const testModel = new GameModel();
+    const testModel = new GameModel(testQuestions);
     testModel._state.answers = [`fast`];
-    testModel.calcPoints();
     assert.equal(2, testModel.calcPoints());
   });
 
   it(`should return 1 if answer is correct`, () => {
-    const testModel = new GameModel();
+    const testModel = new GameModel(testQuestions);
     testModel._state.answers = [`correct`];
     assert.equal(1, testModel.calcPoints());
   });
 
   it(`should return 0 if answers are fast and wrong`, () => {
-    const testModel = new GameModel();
+    const testModel = new GameModel(testQuestions);
     testModel._state.answers = [`fast`, `wrong`];
     assert.equal(0, testModel.calcPoints());
   });
 
   it(`should return -1 if wrong answers are more then others`, () => {
-    const testModel = new GameModel();
+    const testModel = new GameModel(testQuestions);
     testModel._state.answers = [`wrong`, `wrong`, `wrong`, `wrong`, `fast`, `fast`, `fast`, `wrong`, `wrong`, `wrong`];
     assert.equal(-1, testModel.calcPoints());
   });
 
   it(`should return 10 if all answers are correct`, () => {
-    const testModel = new GameModel();
+    const testModel = new GameModel(testQuestions);
     testModel._state.answers = [`correct`, `correct`, `correct`, `correct`, `correct`, `correct`, `correct`, `correct`, `correct`, `correct`];
     assert.equal(10, testModel.calcPoints());
   });
 
   it(`should return 5 if 3 answers are wrong, 4 answers are fast and 3 is correct`, () => {
-    const testModel = new GameModel();
+    const testModel = new GameModel(testQuestions);
     testModel._state.answers = [`fast`, `wrong`, `correct`, `fast`, `wrong`, `correct`, `fast`, `wrong`, `correct`, `fast`];
     assert.equal(5, testModel.calcPoints());
   });
@@ -141,22 +142,24 @@ describe(`Get minutes and seconds from seconds`, () => {
 });
 
 describe(`Calc answers type`, () => {
+  const testQuestions = [{'question': 12}, {'question': 13}, {'question': 12}];
+
   it(`Shouls return 3 wrong answers`, () => {
-    const testModel = new GameModel();
+    const testModel = new GameModel(testQuestions);
     testModel._state.answers = [`fast`, `correct`, `wrong`, `fast`, `correct`, `wrong`, `fast`, `correct`, `wrong`, `fast`];
 
     assert.equal(3, testModel.calcAnswersType(`wrong`));
   });
 
   it(`Shouls return 3 correct answers`, () => {
-    const testModel = new GameModel();
+    const testModel = new GameModel(testQuestions);
     testModel._state.answers = [`fast`, `correct`, `wrong`, `fast`, `correct`, `wrong`, `fast`, `correct`, `wrong`, `fast`];
 
     assert.equal(3, testModel.calcAnswersType(`correct`));
   });
 
   it(`Shouls return 0 correct answers`, () => {
-    const testModel = new GameModel();
+    const testModel = new GameModel(testQuestions);
     testModel._state.answers = [`fast`, `wrong`, `fast`, `wrong`, `fast`, `wrong`, `fast`];
 
     assert.equal(0, testModel.calcAnswersType(`correct`));
